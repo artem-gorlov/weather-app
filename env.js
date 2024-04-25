f9cd93e0973ec39d1281c69a0ac37c51492644df
Artyom Gorlov <nanase@MacBook-Air-Artyom.local>
2024-04-25 19:07:28 +0400

    fix env file

---
+++ env.js
@@ -0,0 +1,6 @@
+
+  export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5/weather"
+  export const WEATHER_API_KEY = "1b96cfaf0d606739824a29b89646f699"
+  export const IMAGE_API_URL = "https://api.unsplash.com/search/photos"
+  export const IMAGE_ACCESS_KEY = "nNSJLwgjjT5vUi6t1k9e5uq8xhM71vQ7Dq65yEf93QI"
+  
